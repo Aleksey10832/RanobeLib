2d1b4c9ff14673ec36dfb721dc1f54f30d5c08eb
aleksey10832 <akuratov47@gmail.com>
2026-07-09 17:22:10 +0600

    c# update start

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using AngleSharp;
+using AngleSharp.Dom;
+
+var config = Configuration.Default.WithDefaultLoader();
+var context = BrowsingContext.New(config);
+string ranobeName = "ranobe174";
+var document = await context.OpenAsync($"https://ranobe.me/{ranobeName}");
+var pages = document.QuerySelectorAll(".FicContentsChapterName");
+foreach(var page in pages)
+{
+    System.Console.WriteLine(page.QuerySelector("a").Flags);
+}
--- novels/chapters.js
+++ novels/chapters.js
@@ -4,5 +4,5 @@ const title_name = (new URLSearchParams(document.location.search)).get('name')
 const chapters = data.find(el => title_name == el.name)
 document.querySelector("#title").innerHTML = title_name.replace('9121', ':').replace('9120', '?')
 chapters.chapters.forEach(element => {
-    chapters_html.insertAdjacentHTML('afterend', `<li><a href="./${element}.html">${element.split('/')[2].replace('9121', ':').replace('9120', '?')}</a></li>`)
+    chapters_html.insertAdjacentHTML('beforeend', `<li><a href="./${element}.html">${element.split('/')[2].replace('9121', ':').replace('9120', '?')}</a></li>`)
 });
