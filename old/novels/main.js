227d1977436694dc68b776061085f42e97b448c6
aleksey10832 <akuratov47@gmail.com>
2026-09-03 18:36:06 +0600

    fix(trash) - delete trash files

--- old/novels/main.js
+++
@@ -1,6 +0,0 @@
-import data from './data.js'
-const titles = document.querySelector('#titles')
-
-data.forEach(element => {
-    titles.insertAdjacentHTML('beforebegin', `<a href="./novels/?name=${element.name}">${element.name.replace('9121', ':').replace('9120', '?')}</a>`) 
-});
